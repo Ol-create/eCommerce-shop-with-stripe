fe4bb25443b25924cd2a5ba98dc7bdbd853932c1
Oluyemi Paul <oluola96@gmail,com>
2022-09-12 01:01:30 +0100

    Feat: add styles and icon to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Search } from '@material-ui/icons';
 
 const Container = styled.div`
   height: 60px;
 
 `
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+`
 
 const Wrapper = styled.div`
   padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`
+const Center = styled.div`
+  flex: 1;
+`
+const Right = styled.div`
+  flex: 1;
 `
 
 const Navbar = ()=> {
     return(
         <Container>
-            <Wrapper> Navbar</Wrapper> 
+            <Wrapper>
+                <Left>
+                    <Language>En</Language>
+                    <SearchContainer>
+                        Input
+                    <Search />
+                    </SearchContainer>
+                </Left>
+                <Center>Center</Center>
+                <Right>Right</Right>
+            </Wrapper> 
         </Container>
     )
 };
